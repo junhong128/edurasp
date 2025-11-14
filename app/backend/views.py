48ae026171327cc617ab6bef5057331a309b2a0d
Jun Hong <ong.junhong888@gmail.com>
2025-11-14 17:33:43 -0500

    added format.html

--- app/backend/views.py
+++ app/backend/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+
+def calculateXYZ(a, b):
+    x = a + b
+    y = a - b
+    z = a * b
+    return x, y, z
 
 def test(request):
-    return HttpResponse("Hello world!")
-
+    x = calculateXYZ(5, 3)
+    template = loader.get_template('format.html', x)
+    return HttpResponse(template.render())
