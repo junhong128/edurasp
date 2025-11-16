6d9493bfc3f159bd1a732de4dd39a9becf92b13c
xsy <xsy@lawn-143-215-94-249.lawn.gatech.edu>
2025-11-15 22:02:20 -0500

    ui design

--- app/backend/views.py
+++ app/backend/views.py
@@ -2,13 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 
-def calculateXYZ(a, b):
-    x = a + b
-    y = a - b
-    z = a * b
-    return x, y, z
-
 def test(request):
-    x = calculateXYZ(5, 3)
-    template = loader.get_template('format.html', x)
+    template = loader.get_template('format.html')
     return HttpResponse(template.render())
